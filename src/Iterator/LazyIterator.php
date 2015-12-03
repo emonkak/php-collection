15205042f981e006d083e75d7fc63a2582bba4e5
Shota Nozaki <emonkak@gmail.com>
2015-12-03 22:12:14 +0900

    Avoid "Cannot rewind a generator that was already run" Exception

--- src/Iterator/LazyIterator.php
+++ src/Iterator/LazyIterator.php
@@ -4,18 +4,41 @@ namespace Emonkak\Collection\Iterator;
 
 use Emonkak\Collection\Utils\Iterators;
 
-class LazyIterator implements \IteratorAggregate
+class LazyIterator implements \Iterator
 {
     private $factory;
+
+    private $it;
 
     public function __construct(callable $factory)
     {
         $this->factory = $factory;
     }
 
-    public function getIterator()
+    public function current()
+    {
+        return $this->it->current();
+    }
+
+    public function key()
+    {
+        return $this->it->key();
+    }
+
+    public function next()
+    {
+        $this->it->next();
+    }
+
+    public function rewind()
     {
         $factory = $this->factory;
-        return Iterators::create($factory());
+        $this->it = Iterators::create($factory());
+        $this->it->rewind();
+    }
+
+    public function valid()
+    {
+        return $this->it->valid();
     }
 }
